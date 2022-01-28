24802230919a8fd1ab84db9a0dc87faa4073cf7d
thddlmy <thddlmy@gmail.com>
2022-01-28 17:36:41 +0900

    study: mini project 3/25

---
+++ QuoteGenerator/script.ts
@@ -0,0 +1,20 @@
+const quotes = [
+  'Think like a man of action and act like man of thought.',
+  'Courage is very important. Like a muscle, it is strengthened by use.',
+  'Life is the art of drawing sufficient conclusions from insufficient premises.',
+  'By doubting we come at the truth.',
+  'A man that hath no virtue in himself, ever envieth virtue in others.',
+  'When money speaks, the truth keeps silent.',
+  'Better the last smile than the first laughter.',
+  'In the morning of life, work; in the midday, give counsel; in the evening, pray.',
+  'Painless poverty is better than embittered wealth.',
+  'A poet is the painter of the soul.',
+];
+
+const $root3 = document.querySelector('#root') as HTMLDivElement;
+const $clickBtn3 = document.querySelector('#btn') as HTMLButtonElement;
+const $message = document.querySelector('#message') as HTMLParagraphElement;
+
+$clickBtn3.addEventListener('click', () => {
+  $message.innerHTML = quotes[Math.floor(Math.random() * 10)];
+});
